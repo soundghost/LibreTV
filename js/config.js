0eab0e862454331ace74acbed9fccb88591bb341
BusDriver <24316853+soundghost@users.noreply.github.com>
2025-08-19 14:06:45 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -114,11 +114,6 @@ const API_SITES = {
         api: 'https://cj.lziapi.com/api.php/provide/vod/',
         name: '量子资源站'
     },
-    testSource: {
-        api: 'https://www.example.com/api.php/provide/vod',
-        name: '空内容测试源',
-        adult: true
-    }
 };
 
 // 定义合并方法
